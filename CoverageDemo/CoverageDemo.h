2e943a41067add0788439bf8393a25bed5dcb634
gabrielCaldeiraMartins <gabriel.martins@caf.io>
2023-06-14 10:54:19 -0300

    Initial Commit

---
+++ CoverageDemo/CoverageDemo.h
@@ -0,0 +1,18 @@
+//
+//  CoverageDemo.h
+//  CoverageDemo
+//
+//  Created by Gabriel Caldeira Martins on 14/06/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CoverageDemo.
+FOUNDATION_EXPORT double CoverageDemoVersionNumber;
+
+//! Project version string for CoverageDemo.
+FOUNDATION_EXPORT const unsigned char CoverageDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoverageDemo/PublicHeader.h>
+
+
